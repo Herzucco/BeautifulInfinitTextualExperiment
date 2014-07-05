9bc84dd052ea0d8463f3c9f7027dbd459766163e
Herzucco <herzucco@gmail.com>
2014-07-05 17:58:11 +0200

    destroy fragment

--- Project/src/com/stratos/beautifulinfinittextualexperiment/MainActivity.java
+++ Project/src/com/stratos/beautifulinfinittextualexperiment/MainActivity.java
@@ -23,11 +23,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        } 
         SharedPreferences prefs = getSharedPreferences(getApplicationInfo().name, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
         
@@ -54,22 +49,6 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
     }
     
     private class AskConnectTask extends
